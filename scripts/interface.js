844fb32fec96ee35c78add6044b845180680f5c0
TorchHollow <nikolasjuliogarciadejesus@gmail.com>
2023-09-01 19:41:08 -0300

    Terminei de fazer o jogo, falta aprimora-lo

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,29 +1,48 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     let quadrados = document.querySelectorAll(".quadrados")
 
-    quadrados.forEach((quadrado)=>{
+    quadrados.forEach((quadrado) => {
         quadrado.addEventListener('click', handleClick)
     })
 })
 
-function handleClick(evento){
+function handleClick(evento) {
     let quadrado = evento.target
     let position = quadrado.id
 
-    handleMove(position)
+    if (handleMove(position)) {
+        setTimeout(() => {
+
+        }, 10)
+
+        alert(" O Jogo Acabou - O Vencedor foi o jogador" + veizDoJogador)
+    }
     updateSquares()
 }
 
-function updateSquares(){
+function updateSquare(position){
+    let quadrado = document.getElementById(position.toString())
+    let simbulos = tabuleiro[position]
+    quadrado.innerHTML = `<div class='${simbulos}'></div>`
+}
+
+function updateSquares() {
 
     let quadrados = document.querySelectorAll(".quadrados")
 
-    quadrados.forEach((quadrado)=>{
+    quadrados.forEach((quadrado) => {
         let position = quadrado.id
         let simbulos = tabuleiro[position]
 
-        if(simbulos != '') {
+        if (simbulos != '') {
             quadrado.innerHTML = `<div class='${simbulos}'></div>`
         }
     })
 }
+
+/*O que falta:
+
+-Fazer um botão de reset
+-Melhorar a mensagem de vitória
+
+*/
--- scripts/jogo.js
+++ scripts/jogo.js
@@ -1,19 +1,55 @@
 //iniciar variáveis
 let tabuleiro = ['', '', '', '', '', '', '', '', '']
 let veizDoJogador = 0
+let gameOver = false
+
 let simbulos = ['o', 'x']
 
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 function handleMove(position) {
+
+    if (gameOver) {
+        return
+    }
 
     if (tabuleiro[position] == '') {
         tabuleiro[position] = simbulos[veizDoJogador]
 
-        if (veizDoJogador == 0) {
-            veizDoJogador = 1
+        gameOver = isWin()
 
-        } else {
-            veizDoJogador = 0
+        if (!gameOver) {
+
+            veizDoJogador = (veizDoJogador == 0) ? 1 : 0
         }
-
     }
+
+    return gameOver
+}
+
+function isWin() {
+
+    for (let i = 0; i < winStates.length; i++) {
+        let seq = winStates[i]
+
+        let pos1 = seq[0]
+        let pos2 = seq[1]
+        let pos3 = seq[2]
+
+        if (tabuleiro[pos1] == tabuleiro[pos2] &&
+            tabuleiro[pos1] == tabuleiro[pos3] &&
+            tabuleiro[pos1] != '') {
+            return true
+        }
+    }
+    return false
 }
